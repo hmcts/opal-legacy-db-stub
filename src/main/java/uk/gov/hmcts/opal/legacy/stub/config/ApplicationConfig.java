0bb4e115a67938edd88188ff2611998ee4e20546
Dan Lysiak <danlysiak@hotmail.com>
2025-08-05 20:05:55 +0100

    update

--- src/main/java/uk/gov/hmcts/opal/legacy/stub/config/ApplicationConfig.java
+++ src/main/java/uk/gov/hmcts/opal/legacy/stub/config/ApplicationConfig.java
@@ -9,6 +9,7 @@ import java.net.http.HttpClient;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
