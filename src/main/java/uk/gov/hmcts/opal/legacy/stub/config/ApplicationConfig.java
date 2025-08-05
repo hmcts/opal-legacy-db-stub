47c032fa89783702f7b7dc0162d9c87c35698b60
Dan Lysiak <danlysiak@hotmail.com>
2025-08-05 19:07:13 +0100

    update

--- src/main/java/uk/gov/hmcts/opal/legacy/stub/config/ApplicationConfig.java
+++ src/main/java/uk/gov/hmcts/opal/legacy/stub/config/ApplicationConfig.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.net.http.HttpClient;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 
 @Configuration
 public class ApplicationConfig {
@@ -16,8 +21,20 @@ public class ApplicationConfig {
     }
 
     @Bean(name = "httpClient")
-    public HttpClient httpClient() {
-        return HttpClient.newHttpClient();
+    public HttpClient httpClient() throws Exception {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) { }
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+                }
+        };
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, new SecureRandom());
+
+        return HttpClient.newBuilder()
+                .sslContext(sslContext)
+                .build();
     }
 
     @Bean(name = "objectMapper")
