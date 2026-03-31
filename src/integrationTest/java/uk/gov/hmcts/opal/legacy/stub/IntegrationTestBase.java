2f661fd17ea99c0feb352464842bcfd8588db400
Russell Dodd <russell.dodd@hmcts.net>
2026-03-31 11:08:35 +0100

    Upgrade to Spring Boot v4

--- src/integrationTest/java/uk/gov/hmcts/opal/legacy/stub/IntegrationTestBase.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/legacy/stub/IntegrationTestBase.java
@@ -1,15 +1,9 @@
 package uk.gov.hmcts.opal.legacy.stub;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTestBase {
@@ -25,9 +19,6 @@ public class IntegrationTestBase {
     public void setUp() {
         baseUrl = baseUrl + ":" + port;
         restTemplate = new RestTemplate();
-        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
-        messageConverters.add(new MappingJackson2HttpMessageConverter(new ObjectMapper()));
-        restTemplate.setMessageConverters(messageConverters);
     }
 
 }
