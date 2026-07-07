4c0d75fc46bbcfef1198368e7bbbf1fd2a10da90
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2026-07-07 16:56:21 +0100

    Revert update and supress cve until stable build

--- src/main/java/uk/gov/hmcts/opal/legacy/stub/config/WireMockServerConfig.java
+++ src/main/java/uk/gov/hmcts/opal/legacy/stub/config/WireMockServerConfig.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.opal.legacy.stub.config;
 
-import com.github.jknack.handlebars.Helper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.extension.Extension;
@@ -13,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import wiremock.com.github.jknack.handlebars.Helper;
 
 import java.io.File;
 import java.util.Map;
